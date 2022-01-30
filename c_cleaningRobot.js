7e199ba7fa001e51d4659a27282695f25a3128a2
Samir Carvalho <samircarvalho00@gmail.com>
2022-01-30 17:11:16 -0300

    adicionando random state wet floor

--- c_cleaningRobot.js
+++ c_cleaningRobot.js
@@ -59,7 +59,15 @@ function makeDiagram(selector) {
                 .on('click', function () {
                     world.markFloorDirty(floorNumber);
                     diagram.floors[floorNumber].attr('class', 'dirty floor');
+                })
+                // adicionando wet floor
+                .on('contextmenu', function() {
+                    if (world.markFloorWet(floorNumber)) {
+                        diagram.floors[floorNumber].attr('class', 'wet floor');
+                    }
                 });
+                
+                
     }
     return diagram;
 }
@@ -128,7 +136,30 @@ function makeAgentControlledDiagram() {
         renderWorld(diagram);
         renderAgentPercept(diagram, percept);
         renderAgentAction(diagram, action);
+
+        randomState();
     }
+
+    function randomState() {
+        if (AUTO_RANDOM_STATE) {
+            let max = 3, min = 0;
+            let state = Math.floor(Math.random() * (max - min + 1)) + min;
+            let floorNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            if (floorNumber != diagram.world.location) {
+                if (state % 2 == 0) {
+                    if (diagram.world.markFloorDirty(floorNumber)) {
+                        diagram.floors[floorNumber].attr('class', 'dirty floor');
+                    }
+                }
+                else {
+                    if (diagram.world.markFloorWet(floorNumber)) {
+                        diagram.floors[floorNumber].attr('class', 'wet floor');
+                    }
+                }
+            }
+        }
+    }
+
     update();
     setInterval(update, STEP_TIME_MS);
 }
@@ -267,6 +298,73 @@ function update() {
      }
  }
  */
+
+ //////////random
+
+const Y_SPACE = 120;
+var AUTO_RANDOM_STATE = true;
+var ALLOW_MANUAL_INPUT = false;
+
+/* Control configurations */
+function switchAutoRandomState() {
+    AUTO_RANDOM_STATE = !AUTO_RANDOM_STATE;
+}
+
+function switchAllowManualInput() {
+    ALLOW_MANUAL_INPUT = !ALLOW_MANUAL_INPUT;
+}
+
+
+
+
+
+
+
+$("body").keydown(function(e){
+    let location;
+    let nextLocation;
+    switch (e.which) {
+        case 13: // return 
+            switchAutoRandomState();
+            alert('RANDOM: ' + {true: 'on', false: 'off'}[AUTO_RANDOM_STATE]);
+            break;
+        case 32: // space
+            switchAllowManualInput();
+            alert('INPUT: ' + {true: 'on', false: 'off'}[ALLOW_MANUAL_INPUT]);
+            break;
+        case 37: // left
+            location = diagram.world.location - 1;
+            nextLocation = {0: null, 1: 0, 2: 3, 3: null}[location];
+            if (nextLocation != null) {
+                diagram.robot.style('transform', `translate(${xPosition(nextLocation)}px,${yPosition(nextLocation)-Y_SPACE}px)`);
+            }
+            break;
+        case 38: // up
+            location = diagram.world.location - 1;
+            nextLocation = {0: 3, 1: 2, 2: null, 3: null}[location];
+            if (nextLocation != null) {
+                diagram.robot.style('transform', `translate(${xPosition(nextLocation)}px,${yPosition(nextLocation)-Y_SPACE}px)`);
+            }
+            break;
+        case 39: // right
+            location = diagram.world.location - 1;
+            nextLocation = {0: 1, 1: null, 2: null, 3: 2}[location];
+            if (nextLocation != null) {
+                diagram.robot.style('transform', `translate(${xPosition(nextLocation)}px,${yPosition(nextLocation)-Y_SPACE}px)`);
+            }
+            break;
+        case 40: // down
+            location = diagram.world.location - 1;
+            nextLocation = {0: null, 1: null, 2: 1, 3: 0}[location];
+            if (nextLocation != null) {
+                diagram.robot.style('transform', `translate(${xPosition(nextLocation)}px,${yPosition(nextLocation)-Y_SPACE}px)`);
+            }
+            break;
+    }
+});
+
+
+
 
 
 makeAgentControlledDiagram();
