f68b5150a63f57986904ca7a75673c362404eb16
Samir Carvalho <samircarvalho00@gmail.com>
2022-02-02 15:29:27 -0300

    melhorando codigo retirando variavel weights

--- cleaningRobot.js
+++ cleaningRobot.js
@@ -6,11 +6,11 @@ class World {
         this.floors = [];
 
         //adicionando pesos
-        this.weights = [];
+//        this.weights = [];
 
         for (let i = 0; i < numFloors; i++) {
-            this.floors.push({dirty: false});
-            this.weights.push(0);
+            this.floors.push({ dirty: false });
+           // this.weights.push(0);
         }
         //
         this.lastClean;
@@ -23,47 +23,55 @@ class World {
     // modificando as ações para adicionar a ultima limpesa e os pesos
     // this.lastClean e this.weights
     simulate(action) {
-        switch(action) {
-        case 'SUCK':
-            this.floors[this.location].dirty = false;
-            this.lastClean = this.location;
-			this.weights[this.location] += 1;
-            break;
-        
+        switch (action) {
+            case 'SUCK':
+                this.floors[this.location].dirty = false;
+                this.lastClean = this.location;
+                //this.weights[this.location] += 1;
+                break;
+
             //alterando a logica para adicionar mais location e os pesos
             case 'LEFT':
-                if(this.location == 1){
+                if (this.location == 1) {
                     this.location = 0;
-                } else if(this.location == 3){
+                } else if (this.location == 3) {
                     this.location = 2;
                 }
-                if (this.weights[this.location] > 0){ this.weights[this.location] -= 1; }
+                //if (this.weights[this.location] > 0) { 
+                //    this.weights[this.location] -= 1; 
+                //}
                 break;
-            
+
             case 'RIGHT':
-                if(this.location == 0){
+                if (this.location == 0) {
                     this.location = 1;
-                } else if (this.location == 2){
+                } else if (this.location == 2) {
                     this.location = 3;
                 }
-                if (this.weights[this.location] > 0){ this.weights[this.location] -= 1; }
+                //if (this.weights[this.location] > 0) { 
+                  //  this.weights[this.location] -= 1; 
+                //}
                 break;
-            
+
             case 'UP':
-                if(this.location == 2){
+                if (this.location == 2) {
                     this.location = 0;
-                } else if (this.location == 3){
+                } else if (this.location == 3) {
                     this.location = 1;
                 }
-                if (this.weights[this.location] > 0){ this.weights[this.location] -= 1; }
+                //if (this.weights[this.location] > 0) { 
+                //    this.weights[this.location] -= 1; 
+                //}
                 break;
             case 'DOWN':
-                if(this.location == 0){
+                if (this.location == 0) {
                     this.location = 2;
-                } else if (this.location == 1){
+                } else if (this.location == 1) {
                     this.location = 3;
                 }
-                if (this.weights[this.location] > 0){ this.weights[this.location] -= 1; }
+                //if (this.weights[this.location] > 0) { 
+                 //   this.weights[this.location] -= 1; 
+               // }
                 break;
             /*
                 case 'DIAG_DOWN':
@@ -84,8 +92,8 @@ class World {
                 if (this.weights[this.location] > 0){ this.weights[this.location] -= 1; }
                 break;
                 */
-            }
-            return action;
+        }
+        return action;
     }
 }
 
@@ -101,101 +109,105 @@ function reflexVacuumAgent(world) {
 //novas regras definidas para os reflex do codigo
 // Rules are defined in code
 function reflexVacuumAgent(world) {
-	if (world.floors[world.location].dirty) { return 'SUCK' }
-    else if (world.location == 0){
-		
-		if(this.lastLocation == 1){
-			this.lastLocation = 0;
-			//if(world.weights[3] > world.weights[2]){
-			//	return 'DIAG_DOWN'
-			//} else { return 'DOWN' }
+    if (world.floors[world.location].dirty) { return 'SUCK' }
+    else if (world.location == 0) {
+
+        if (this.lastLocation == 1) {
+            this.lastLocation = 0;
+            //if(world.weights[3] > world.weights[2]){
+            //	return 'DIAG_DOWN'
+            //} else { return 'DOWN' }
             return 'DOWN'
 
-		} else if(this.lastLocation == 2) { 
-			this.lastLocation = 0;
+        } else if (this.lastLocation == 2) {
+            this.lastLocation = 0;
 
-			//if(world.weights[3] > world.weights[1]){
-			//	return 'DIAG_DOWN'
-			//} else { return 'RIGHT' }
+            //if(world.weights[3] > world.weights[1]){
+            //	return 'DIAG_DOWN'
+            //} else { return 'RIGHT' }
             return 'RIGHT'
-		
-		} else { 
-			this.lastLocation = 0;
-			
-			if(world.weights[1] > world.weights[2]){
-				return 'RIGHT'
-			} else { return 'DOWN' }
-		}
-	}
-    else if (world.location == 1){
-		if(this.lastLocation == 3) { 
-			this.lastLocation = 1;
 
-			//if(world.weights[2] > world.weights[0]) {
-			//	return 'DIAG_DOWN'
-			//} else { return 'LEFT' }
-            return 'LEFT'
-		
-		} else if (this.lastLocation == 0) { 
-			this.lastLocation = 1;
-			
-			//if(world.weights[2] > world.weights[3]){
-			//	return 'DIAG_DOWN'
-			//} else { return 'DOWN' }
+        } else {
+            this.lastLocation = 0;
+
+            //	if(world.weights[1] > world.weights[2]){
+            //		return 'RIGHT'
+            //	} else { return 'DOWN' }
             return 'DOWN'
-		} else {
-			this.lastLocation = 1;
-			
-			if(world.weights[0] > world.weights[3]){
-				return 'LEFT'
-			} else { return 'DOWN' }
-		}
-	}
-	else if (world.location == 2){
-		if(this.lastLocation == 0) { 
-			this.lastLocation = 2;
-
-			//if(world.weights[1] > world.weights[3]){
-			//	return 'DIAG_UP'
-			//} else { return 'RIGHT' }
-            return 'RIGHT'
-		} else if(this.lastLocation == 3) { 
-			this.lastLocation = 2;
-			
-			//if(world.weights[1] > world.weights[0]){
-			//	return 'DIAG_UP'
-			//} else { return 'UP' }
-            return 'UP'
-		} else {
-			this.lastLocation = 2;
-			
-			if(world.weights[3] > world.weights[0]){
-				return 'RIGHT'
-			} else { return 'UP' }	
-		}
+        }
     }
-	else if (world.location == 3){
-		if(this.lastLocation == 1) { 
-			this.lastLocation = 3;
+    else if (world.location == 1) {
+        if (this.lastLocation == 3) {
+            this.lastLocation = 1;
 
-	//		if(world.weights[0] > world.weights[2]){
-	//			return 'DIAG_UP'
-	//		} else { return 'LEFT' }
-		return 'LEFT'
-		} else if(this.lastLocation == 2) { 
-			this.lastLocation = 3;
-			
-		//	if(world.weights[0] > world.weights[1]){
-		//		return 'DIAG_UP'
-		//	} else { return 'UP' }
+            //if(world.weights[2] > world.weights[0]) {
+            //	return 'DIAG_DOWN'
+            //} else { return 'LEFT' }
+            return 'LEFT'
+
+        } else if (this.lastLocation == 0) {
+            this.lastLocation = 1;
+
+            //if(world.weights[2] > world.weights[3]){
+            //	return 'DIAG_DOWN'
+            //} else { return 'DOWN' }
+            return 'DOWN'
+        } else {
+            this.lastLocation = 1;
+
+            //	if(world.weights[0] > world.weights[3]){
+            //			return 'LEFT'
+            //		} else { return 'DOWN' }
+            return 'DOWN'
+        }
+    }
+    else if (world.location == 2) {
+        if (this.lastLocation == 0) {
+            this.lastLocation = 2;
+
+            //if(world.weights[1] > world.weights[3]){
+            //	return 'DIAG_UP'
+            //} else { return 'RIGHT' }
+            return 'RIGHT'
+        } else if (this.lastLocation == 3) {
+            this.lastLocation = 2;
+
+            //if(world.weights[1] > world.weights[0]){
+            //	return 'DIAG_UP'
+            //} else { return 'UP' }
             return 'UP'
-    } else {
-			this.lastLocation = 3;
-			
-			if(world.weights[2] > world.weights[1]) {
-				return 'LEFT'
-			} else { return 'UP' }
-		}
+        } else {
+            this.lastLocation = 2;
+
+            //	if(world.weights[3] > world.weights[0]){
+            //			return 'RIGHT'
+            //		} else { return 'UP' }	
+            return 'UP'
+        }
+    }
+    else if (world.location == 3) {
+        if (this.lastLocation == 1) {
+            this.lastLocation = 3;
+
+            //		if(world.weights[0] > world.weights[2]){
+            //			return 'DIAG_UP'
+            //		} else { return 'LEFT' }
+            return 'LEFT'
+        } else if (this.lastLocation == 2) {
+            this.lastLocation = 3;
+
+            //	if(world.weights[0] > world.weights[1]){
+            //		return 'DIAG_UP'
+            //	} else { return 'UP' }
+            return 'UP'
+        } else {
+            this.lastLocation = 3;
+
+            //if(world.weights[2] > world.weights[1]) {
+            //		return 'LEFT'
+            //	} else { return 'UP' }
+            return 'UP'
+        }
     }
 }
 /*
